9c4c34ff29976eb8846e41229c55801924a4886d
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2025-01-15 10:54:41 +0600

    banner button and other design added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,28 +1,146 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
-import "./Banner.css"
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import "./Banner.css";
+import Container from "../../../Components/Container/Container";
 
 const Banner = () => {
-    return (
-        <div id="banner-part">
-            <div className="banner-main">
-            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+  return (
+    <div id="banner-part">
+      <div className="banner-main">
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          <SwiperSlide>
+            <div className="img1">
+              <div className="overlay">
+              <Container>
+                <div className="banner-text px-3">
+                  <h1 className="lg:text-7xl md:text-4xl sm:text-2xl font-bold ">Affordable <br /> Price For Car <br /> Servicing</h1>
+                  <p className="lg:text-3xl md:text-2xl sm:text-1xl font-normal mt-5">
+                    There are many variations of passages of available, but <br /> the
+                    majority have suffered alteration in some form
+                  </p>
+
+                  <div className="banner-btn mt-5">
+                  <button className="btn1  ">Discover More</button>
+                  <button className="btn2 ms-2">Latest Project</button>
+                  </div>
+                </div>
+              </Container>
+              </div>
+
             </div>
-        </div>
-    );
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="img2">
+              <div className="overlay">
+              <Container>
+                <div className="banner-text px-3">
+                  <h1 className="lg:text-7xl md:text-4xl sm:text-2xl font-bold ">Affordable <br /> Price For Car <br /> Servicing</h1>
+                  <p className="lg:text-3xl md:text-2xl sm:text-1xl font-normal mt-5">
+                    There are many variations of passages of available, but <br /> the
+                    majority have suffered alteration in some form
+                  </p>
+
+                  <div className="banner-btn mt-5">
+                  <button className="btn1  ">Discover More</button>
+                  <button className="btn2 ms-2">Latest Project</button>
+                  </div>
+                </div>
+              </Container>
+              </div>
+
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="img3">
+              <div className="overlay">
+              <Container>
+                <div className="banner-text px-3">
+                  <h1 className="lg:text-7xl md:text-4xl sm:text-2xl font-bold ">Affordable <br /> Price For Car <br /> Servicing</h1>
+                  <p className="lg:text-3xl md:text-2xl sm:text-1xl font-normal mt-5">
+                    There are many variations of passages of available, but <br /> the
+                    majority have suffered alteration in some form
+                  </p>
+
+                  <div className="banner-btn mt-5">
+                  <button className="btn1  ">Discover More</button>
+                  <button className="btn2 ms-2">Latest Project</button>
+                  </div>
+                </div>
+              </Container>
+              </div>
+
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="img4">
+              <div className="overlay">
+              <Container>
+                <div className="banner-text px-3">
+                  <h1 className="lg:text-7xl md:text-4xl sm:text-2xl font-bold ">Affordable <br /> Price For Car <br /> Servicing</h1>
+                  <p className="lg:text-3xl md:text-2xl sm:text-1xl font-normal mt-5">
+                    There are many variations of passages of available, but <br /> the
+                    majority have suffered alteration in some form
+                  </p>
+
+                  <div className="banner-btn mt-5">
+                  <button className="btn1  ">Discover More</button>
+                  <button className="btn2 ms-2">Latest Project</button>
+                  </div>
+                </div>
+              </Container>
+              </div>
+
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="img5">
+              <div className="overlay">
+              <Container>
+                <div className="banner-text px-3">
+                  <h1 className="lg:text-7xl md:text-4xl sm:text-2xl font-bold ">Affordable <br /> Price For Car <br /> Servicing</h1>
+                  <p className="lg:text-3xl md:text-2xl sm:text-1xl font-normal mt-5">
+                    There are many variations of passages of available, but <br /> the
+                    majority have suffered alteration in some form
+                  </p>
+
+                  <div className="banner-btn mt-5">
+                  <button className="btn1  ">Discover More</button>
+                  <button className="btn2 ms-2">Latest Project</button>
+                  </div>
+                </div>
+              </Container>
+              </div>
+
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="img6">
+              <div className="overlay">
+              <Container>
+                <div className="banner-text px-3">
+                  <h1 className="lg:text-7xl md:text-4xl sm:text-2xl font-bold ">Affordable <br /> Price For Car <br /> Servicing</h1>
+                  <p className="lg:text-3xl md:text-2xl sm:text-1xl font-normal mt-5">
+                    There are many variations of passages of available, but <br /> the
+                    majority have suffered alteration in some form
+                  </p>
+
+                  <div className="banner-btn mt-5">
+                  <button className="btn1  ">Discover More</button>
+                  <button className="btn2 ms-2">Latest Project</button>
+                  </div>
+                </div>
+              </Container>
+              </div>
+
+            </div>
+          </SwiperSlide>
+
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
